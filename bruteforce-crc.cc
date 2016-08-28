380d427bf558181128e57cd4c47aeb45ea676ca7
martynp <git@disputedip.com>
2016-08-28 22:14:18 +0100

    Code tidy Added fixed initial value

--- bruteforce-crc.cc
+++ bruteforce-crc.cc
@@ -1,7 +1,13 @@
 /*
  * Brute-force a CRC.
  *
- * Author: Martin Schobert <schobert@sitsec.net>
+ * Original Author: Martin Schobert <schobert@sitsec.net>
+ * Modified by MarytnP <git@disputedip.com>
+ *
+ *    Copyright Martin Schobert and MartynP 2012 - 2016.
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
  *
  */
 #include <sys/time.h>
@@ -46,34 +52,40 @@ struct bruteforce_params {
   unsigned int _width;
   uint32_t _start_poly, _end_poly;
   uint32_t _final_xor;
+  uint32_t _initial;
   bool _ref_in, _ref_out;
   size_t _start, _end; // message offsets
   message_list_t const & _msg_list;
   std::vector<fast_int_t> const & _expected_crcs;
   bool _probe_final_xor;
+  bool _probe_initial;
 
   bruteforce_params(unsigned int width,
 		    uint32_t start_poly, 
 		    uint32_t end_poly,
 		    uint32_t final_xor,
+		    uint32_t initial,
 		    bool ref_in, 
 		    bool ref_out,
 		    size_t start, 
 		    size_t end,
 		    message_list_t const & msg_list,
 		    std::vector<fast_int_t> const & expected_crcs,
-		    bool probe_final_xor) 
+		    bool probe_final_xor,
+			bool probe_initial) 
     : _width(width),
       _start_poly(start_poly),
       _end_poly(end_poly),
       _final_xor(final_xor),
+      _initial(initial),
       _ref_in(ref_in),
       _ref_out(ref_out),
       _start(start),
       _end(end),
       _msg_list(msg_list),
       _expected_crcs(expected_crcs),
-      _probe_final_xor(probe_final_xor) {}
+      _probe_final_xor(probe_final_xor),
+      _probe_initial(probe_initial) {}
 };
 
 int bool_to_int(bool v) { 
@@ -158,40 +170,40 @@ void print_stats() {
 
 bool brute_force(struct bruteforce_params p) {
   
-  crc_t crc(p._width);
-  uint32_t init = 0;
+	// Get a CRC checker
+	crc_t crc(p._width);
+	uint32_t init = 0;
+
+	for(uint32_t poly = p._start_poly; poly < p._end_poly && poly <= MAX_VALUE(p._width); poly++) {
+
+		for(uint32_t final_xor = (p._probe_final_xor ? 0 : p._final_xor); final_xor <= (p._probe_final_xor ? MAX_VALUE(p._width) : p._final_xor); final_xor++) {
+
+			crc.set(poly, 0, final_xor, p._ref_in, p._ref_out);
+
+			for(init = 0; init <= MAX_VALUE(p._width); init++) {
+
+				bool match = true;
+				size_t m_i;
+
+				for(m_i = 0; match && (m_i < p._msg_list.size()); m_i++) {
+					match = crc.calc_crc(init, p._msg_list[m_i], p._start, p._end, p._expected_crcs[m_i]);
+				}
+
+				if(m_i == p._msg_list.size()) {
+					show_hit(poly, init, p);
+					//exit(0);
+				}
+			}
 
 
-  for(uint32_t poly = p._start_poly; poly < p._end_poly && poly <= MAX_VALUE(p._width); poly++) {
+			boost::mutex::scoped_lock mylock(mymutex);
+			crc_counter += MAX_VALUE(p._width);
 
-    for(uint32_t final_xor = (p._probe_final_xor ? 0 : p._final_xor); 
-	final_xor <= (p._probe_final_xor ? MAX_VALUE(p._width) : p._final_xor); 
-	final_xor++) {
-
-      crc.set(poly, 0, final_xor, p._ref_in, p._ref_out);
-
-      for(init = 0; init <= MAX_VALUE(p._width); init++) {
-	bool match = true;
-	size_t m_i;
-	
-	for(m_i = 0; match && (m_i < p._msg_list.size()); m_i++) {
-	  match = crc.calc_crc(init, p._msg_list[m_i], p._start, p._end, p._expected_crcs[m_i]);
+			if(p._probe_final_xor || (poly % 0x80 == 0))
+				print_stats();
+		}
 	}
-	
-	if(m_i == p._msg_list.size()) {
-	  show_hit(poly, init, p);
-	  exit(0);
-	}
-      }
-
-      
-      boost::mutex::scoped_lock mylock(mymutex);
-      crc_counter += MAX_VALUE(p._width);
-      if(p._probe_final_xor || (poly % 0x80 == 0))
-	print_stats();
-    }
-  }
-  return false;
+	return false;
 }
 	    
 
@@ -294,18 +306,28 @@ message_list_t read_file(std::string const& file) {
 
 int main(int argc, char *argv[]) {
 
-  size_t width = 16, offs_crc = 80;
-  size_t start = 0;
-  size_t end = offs_crc;
-  fast_int_t final_xor = 0;
+ 	size_t width = 16;
+	size_t offs_crc = 80;
+	size_t start = 0;
+	size_t end = offs_crc;
 
-  fast_int_t poly = 0, start_poly = 0, end_poly;
-  bool ref_in = false, ref_out = false;
-  bool probe_final_xor = false;
+	fast_int_t poly = 0;
+	fast_int_t start_poly = 0;
+	fast_int_t end_poly;
 
-  // definition of program options
-  po::options_description desc("Allowed options");
-  desc.add_options()
+	bool ref_in = false;
+	bool ref_out = false;
+
+	fast_int_t initial = 0;
+	bool probe_initial = true;
+
+	fast_int_t final_xor = 0;
+	bool probe_final_xor = false;
+
+	// Definition of program options
+	// Boost program options to allow settings with call
+	po::options_description desc("Allowed options");
+	desc.add_options()
     ("help", "produce help message")
     ("file", po::value<std::string>(), "file containing messages")
     ("threads", po::value<unsigned int >(), "number of threads (default: 4)")
@@ -313,111 +335,137 @@ int main(int argc, char *argv[]) {
     ("offs-crc", po::value<size_t>(), "CRC's offset")
     ("start", po::value<size_t>(), "calculate CRC from this offset")
     ("end", po::value<size_t>(), "calculate CRC up to this offset (not included)")
+	("initial", po::value<size_t>(), "set intial value (default: 0)")
+    ("probe-initial", po::value<bool>(), "bruteforce the intial, overrides initial (default: true)")
     ("final-xor", po::value<fast_int_t>(), "final xor (default: 0)")
+    ("probe-final-xor", po::value<bool>(), "bruteforce the final-xor, overrides final-xor (default: false)")
     ("poly", po::value<fast_int_t>(), "truncated poly (default: bruteforced)")
     ("reflect-in", po::value<bool>(), "reflect input (default: false)")
     ("reflect-out", po::value<bool>(), "reflect remainder output (default: false)")
-    ("probe-final-xor", po::value<bool>(), "bruteforce the final-xor, too (default: false)")
     ;
 
-  // parse programm options
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm); 
+	// Parse programm options
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm); 
 
-  
-  if(vm.count("help") || !vm.count("file")) {
-    std::cout << desc << "\n";
-    return 1;
-  }
+	// Handle call for help and non-valid call
+	if(vm.count("help") || !vm.count("file")) {
+		std::cout << desc << std::endl;
+		return 1;
+	}
 
-  if(vm.count("threads")) num_threads = vm["threads"].as<unsigned int>();
-  if(vm.count("width")) width = vm["width"].as<size_t>();
-  if(vm.count("offs-crc")) offs_crc = vm["offs-crc"].as<size_t>();
-  if(vm.count("start")) start = vm["start"].as<size_t>();
-  if(vm.count("end")) end = vm["end"].as<size_t>();
-  if(vm.count("final-xor")) final_xor = vm["final-xor"].as<fast_int_t>();
-  if(vm.count("poly")) poly = vm["poly"].as<fast_int_t>();
-  if(vm.count("reflect-in")) ref_in = vm["reflect-in"].as<bool>();
-  if(vm.count("reflect-out")) ref_out = vm["reflect-out"].as<bool>();
-  if(vm.count("probe-final-xor")) probe_final_xor = vm["probe-final-xor"].as<bool>();
+	// Load inputs to local variables
+	if(vm.count("threads")) num_threads = vm["threads"].as<unsigned int>();
+	if(vm.count("width")) width = vm["width"].as<size_t>();
+	if(vm.count("offs-crc")) offs_crc = vm["offs-crc"].as<size_t>();
+	if(vm.count("start")) start = vm["start"].as<size_t>();
+	if(vm.count("end")) end = vm["end"].as<size_t>();
+	if(vm.count("initial")) initial = vm["initial"].as<fast_int_t>();
+	if(vm.count("probe-initial")) probe_initial = vm["probe-initial"].as<bool>();
+	if(vm.count("final-xor")) final_xor = vm["final-xor"].as<fast_int_t>();
+	if(vm.count("probe-final-xor")) probe_final_xor = vm["probe-final-xor"].as<bool>();
+	if(vm.count("poly")) poly = vm["poly"].as<fast_int_t>();
+	if(vm.count("reflect-in")) ref_in = vm["reflect-in"].as<bool>();
+	if(vm.count("reflect-out")) ref_out = vm["reflect-out"].as<bool>();
 
-  // check parameters
+	// Check parameters TODO: A lot more checking
+	if(width > 16) { std::cout << "maximum value for width is: 16\n"; exit(1); } // Why 16?
 
-  if(width > 16) { std::cout << "maximum value for width is: 16\n"; exit(1); }
+	// Read messages from intput file
+	message_list_t msg_list;
+ 	if(vm.count("file")) {
+		msg_list = read_file(vm["file"].as<std::string>());
+	}
 
-  // read messages
-  message_list_t msg_list;
-  if(vm.count("file")) {
-    msg_list = read_file(vm["file"].as<std::string>());
-  }
-  
+	// Define search space
+	start_poly = poly > 0 ? poly : 0x0000;
+	end_poly = (poly > 0 ? poly : MAX_VALUE(width));
 
-  start_poly = poly > 0 ? poly : 0x0000;
-  end_poly = (poly > 0 ? poly : MAX_VALUE(width));
+	// Override non-conformal input
+	if (probe_initial) initial = 0;
 
-  // print parameter
-  std::cout << "number of threads        : " << num_threads << "\n"
-	    << "width                    : " << width << " bits\n"
-	    << "CRC's offset             : " << offs_crc << "\n"
-	    << "calc CRC for bit offsets : " << start << " .. " << end << " (not included)\n"
-	    << "final XOR                : " << final_xor << "\n"
-	    << "truncated polynom        : from " << start_poly << " to " << end_poly << " (MSB not shown)\n"
-	    << "initial value            : from 0 to " << MAX_VALUE(width) << "\n"
-	    << "probe final xor          : " << bool_to_str(probe_final_xor) << "\n"
-	    << "probe reflect in         : " << bool_to_str(ref_in) << "\n"
-	    << "probe reflect out        : " << bool_to_str(ref_out) << "\n"
-	    << "\n";
-  
+	// Output Brute Force Paramaters
+	std::cout 	<< "number of threads        : " << num_threads << std::endl
+			    << "width                    : " << width << " bits" << std::endl
+			    << "CRC's offset             : " << offs_crc << std::endl
+	    		<< "calc CRC for bit offsets : " << start << " .. " << end << " (not included)" << std::endl
+			    << "final XOR                : " << final_xor << std::endl
+			    << "truncated polynom        : from " << start_poly << " to " << end_poly << " (MSB not shown)" << std::endl;
 
-  // extract expected CRCs for each message
-  extract_expected_crcs_from_messages(expected_crcs, msg_list, offs_crc, width);
+	if (probe_initial)
+		std::cout << "initial value            : from 0 to " << MAX_VALUE(width) << std::endl;
+	else
+		std::cout << "initial value            : " << initial << std::endl;
 
-  // calculate number of crc calculations
-  crc_steps = poly > 0 ? 1 : 1+MAX_VALUE(width); // number of polys
-  crc_steps *= 1+MAX_VALUE(width); // number of inits
-  if(ref_in) crc_steps *= 2;
-  if(ref_out) crc_steps *= 2;
-  if(probe_final_xor) crc_steps *= 1+MAX_VALUE(width);
+	if (probe_final_xor)
+		std::cout << "probe final xor          : " << bool_to_str(probe_final_xor) << std::endl;
+	else
+		std::cout << "final xor          : " << final_xor << std::endl;
 
-  gettimeofday(&start_time, NULL);
-  gettimeofday(&current_time, NULL);
+	std::cout	<< "probe reflect in         : " << bool_to_str(ref_in) << std::endl
+			    << "probe reflect out        : " << bool_to_str(ref_out) << std::endl
+			    << std::endl;
+
+	// Extract expected CRCs for each message
+	extract_expected_crcs_from_messages(expected_crcs, msg_list, offs_crc, width);
+
+	// Calculate number of crc calculations
+	crc_steps = poly > 0 ? 1 : 1+MAX_VALUE(width); // number of polys
+	if (probe_initial)
+		crc_steps *= 1+MAX_VALUE(width); // number of inits
+	if(ref_in) crc_steps *= 2;
+	if(ref_out) crc_steps *= 2;
+	if(probe_final_xor) crc_steps *= 1+MAX_VALUE(width);
+
+	// For statistics
+	gettimeofday(&start_time, NULL);
+	gettimeofday(&current_time, NULL);
 
 
-  if(((end-start) % 8 != 0) || (end - start == 0)) {
-    std::cout << "input reflection only works if range start ... end is N * 8 bit with N > 0\n"; 
-    exit(1);
-  }
+  	if(((end-start) % 8 != 0) || (end - start == 0)) {
+    	std::cout << "input reflection only works if range start ... end is N * 8 bit with N > 0\n"; 
+	 //   exit(1);
+	}
 
-  ThreadPool<boost::function0<void> > pool;
-  int poly_step = poly > 0 ? 1 : MAX_VALUE(width)/num_threads;
+	ThreadPool<boost::function0<void> > pool;
+	int poly_step = poly > 0 ? 1 : MAX_VALUE(width)/num_threads;
 
-  for(int probe_ref_in = 0; probe_ref_in <= bool_to_int(ref_in); probe_ref_in++) {
+	for(int probe_ref_in = 0; probe_ref_in <= bool_to_int(ref_in); probe_ref_in++) {
 
-    for(int probe_ref_out = 0; probe_ref_out <= bool_to_int(ref_out); probe_ref_out++) {
+	    for(int probe_ref_out = 0; probe_ref_out <= bool_to_int(ref_out); probe_ref_out++) {
 
-      for(uint32_t _poly = start_poly; 
-	  _poly <= end_poly; 
-	  _poly += poly_step) {
+			// Step through search space, assigning a batch of polynomials to each thread 
+			// (poly_step polynomials per thread)
+			for(uint32_t _poly = start_poly; _poly <= end_poly; _poly += poly_step) {
 	
-	struct bruteforce_params p(width, _poly, _poly + poly_step, 
-				   final_xor, 
-				   int_to_bool(probe_ref_in), int_to_bool(probe_ref_out),
-				   start, end, msg_list, expected_crcs,
-				   probe_final_xor);
+				struct bruteforce_params p(	width, 
+											_poly, 
+											_poly + poly_step, 
+											final_xor, 
+											initial,
+							   				int_to_bool(probe_ref_in), 
+											int_to_bool(probe_ref_out),
+											start, 
+											end,
+											msg_list, 
+											expected_crcs,
+							   				probe_final_xor,
+											probe_initial);
 	
-	pool.add(boost::bind(&brute_force, p));
-	//std::cout << "XXXX\n";
-      }
-      
-    }
-  }
+				pool.add(boost::bind(&brute_force, p));
 
-  pool.wait();
+			}
 
-  std::cout << "\nNo model found.\n";
+		}
 
-  return 0;
+  	}
+
+	pool.wait();
+
+	std::cout << "\nNo model found.\n";
+
+	return 0;
 }
 
 
